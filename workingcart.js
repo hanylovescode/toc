cb46d8141fc7745b6489300e1f9011baf50ff78a
hanycopes <hanisarduk93@gmail.com>
2021-03-31 06:09:47 +0300

    fixing cart thanks to Supa

--- workingcart.js
+++ workingcart.js
@@ -11,6 +11,28 @@ var totalCartElement = document.getElementById('totalcart')
 var checkout = document.getElementById('checkout')
 // getting the price using a form
 // let price = event.target.form.price.value;
+// for checkout
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+  // TODO: find a way to show the final or total price
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+function myCheckout() {
+  var popup = document.getElementById("checkoutform");
+  popup.classList.toggle("show");
+
+}
+
+// let myForm = document.getElementById("myForm");
+// let checkoutButton = document.getElementById('checkoutButton')
+// checkoutButton.addEventListener("click", (event)=>{
+//   myForm
+//   myForm.classList.toggle("show");
+
+// });
 
 // so prices are dependent on titles if titles exist then prices are set if not
 
@@ -23,69 +45,53 @@ function addToCart() {
     errorElement.innerHTML = "";    
     // let prices = price.slice(1,6);
     let parse_price = parseFloat(price);
-    if (price === "")
+    if (price === ""){
       errorElement.innerHTML = "No prices selected";
-    else
+    }
+    else {
       arrayOfTitles.push(title);
       arrayOfPrices.push(parse_price);
       checkout.innerHTML = arrayOfTitles;
       console.log(arrayOfPrices);
       console.log(arrayOfTitles);
+      if (arrayOfTitles.length == 0) {
+        totalcart.innerHTML = "Cart Empty";
+    }
+      else {
+      
+        let sum=arrayOfPrices.reduce(function(a, b){return a+b;});
+        totalcart.innerHTML = arrayOfTitles + "<br> Total Price: "+ "$"+sum;
+    }
+  }
     price.value = ""; 
   }
 
-function showTotalPrices(){
-    // for the error message if you click total price and there is no input 
-    // console.log(arrayOfPrices)
-    if (arrayOfTitles.length == 0 ) 
-      errorElement.innerHTML = "No prices to show";
-    else
-    // I think the undefined is stemming from sum but how to I avoid this
-        var sum=arrayOfPrices.reduce(function(a, b){return a+b;})
-        totalprice.innerHTML = "$"+sum
-        // how to fix the undefined problem maybe try and catch. if undefined print nothing 
-        // or just if/else statement 
-    // in case I want to add the name of meals 
-    // totalprice.innerHTML = arrayOfPrices.join(' ');
-}
+// function showTotalPrices(){
+//     // for the error message if you click total price and there is no input 
+//     // console.log(arrayOfPrices)
+//     if (arrayOfTitles.length == 0 ) 
+//       errorElement.innerHTML = "No prices to show";
+//     else
+//     // I think the undefined is stemming from sum but how to I avoid this
+//         var sum=arrayOfPrices.reduce(function(a, b){return a+b;})
+//         totalprice.innerHTML = "$"+sum
+//         // how to fix the undefined problem maybe try and catch. if undefined print nothing 
+//         // or just if/else statement 
+//     // in case I want to add the name of meals 
+//     // totalprice.innerHTML = arrayOfPrices.join(' ');
+// }
 // var product=[];
 
-function showCart(){
-  // arrayOfPrices.length == 1 
-  if (arrayOfTitles.length == 0) 
-      errorElement.innerHTML = "Nothing selected yet";
-  else 
-    totalcart.innerHTML = arrayOfTitles
-  // arrayOfTitles.length will give you for example two shrimp meals
-  // 
-      // var iDiv = document.createElement('div');
-      // debugger;
-      // iDiv.id = arrayOfPrices.length;
-      // iDiv.className = 'block';
-      // document.getElementById('totalcart').appendChild(iDiv)
-      // // document.getElementsByTagName('header')[0].appendChild(iDiv);
-      // var para = document.createElement("span");
-      // var node = document.createTextNode('Meal: ' + arrayOfTitles+' |                    ');
-      // para.appendChild(node);
-      // console.log(node)
-      // console.log(para)
+// function showCart(){
+//   // arrayOfPrices.length == 1 
+//   if (arrayOfTitles.length == 0) 
+//       errorElement.innerHTML = "Nothing selected yet";
+//   else 
 
-      // var element = document.getElementById(arrayOfPrices.length);
-      // element.appendChild(para);
-      // console.log(element)
-      // para = document.createElement("span");
-      // node = document.createTextNode('Price: '+ arrayOfPrices);
-      // para.appendChild(node);
-      // console.log(node)
-      // console.log(para)
-      // element.appendChild(para);
-      // console.log(element)
-      // clearNode(node, para)
-    
-}
+//     totalcart.innerHTML = arrayOfTitles
+
+// }
 function clearNode(){
-  // var div = document.getElementById("totalcart");
-  // var emptyPrices = document.getElementById("totalprice")
   totalcart.innerHTML = "";
   totalprice.innerHTML = "";
   checkout.innerHTML = "";
@@ -96,50 +102,33 @@ function clearNode(){
 
 }
 // this clears the window from the cart BUT I also want to remove empty the nodes in the function
-HTMLElement.prototype.hide = function() {
-	this.innerHTML = '';
+// HTMLElement.prototype.hide = function() {
+// 	this.innerHTML = '';
 
-}
-var hide = document.getElementById('hide'),
-  cart = document.getElementById('totalcart');
-
-hide.onclick = function() {
-  cart.hide();
-}
-// HTMLElement.prototype.empty = function() {
-//   var that = this;
-//   while (that.hasChildNodes()) {
-//     that.removeChild(that.lastChild);
-//   }
-// };
-
-
-// function showCart(){
-//   var x={};
-// //   the problem is if you get ElementById this will have same results to everything
-// // 
-//   x.price=document.getElementById('price').innerHTML;
-//   x.title=document.getElementById('title').innerHTML;
-//   product.push(x);
-// // this creates an element div ;
-//   var iDiv = document.createElement('div');
-// //   this is the id of the element
-//   iDiv.id = product.length;
-// //   this is the classname of the element
-//   iDiv.className = 'block';
-// //   this add the iDiv to the end of the page
-// //   document.getElementsByTagName('body')[0].appendChild(iDiv);
-// document.getElementById('totalcart').appendChild(iDiv)
-//   var para = document.createElement("span");
-//   var node = document.createTextNode('Meal: ' + x.title+' |                    ');
-//   para.appendChild(node);
-
-//   var element = document.getElementById(product.length);
-//   element.appendChild(para);
-  
-//   para = document.createElement("span");
-//   node = document.createTextNode('Price: '+ x.price);
-//   para.appendChild(node);
-  
-//   element.appendChild(para);
 // }
+// var hide = document.getElementById('hide'),
+//   cart = document.getElementById('totalcart');
+
+// hide.onclick = function() {
+//   cart.hide();
+// }
+// let myForm = document.getElementById("myForm")
+// let checkoutButton = document.getElementById('checkoutButton')
+// checkoutButton.addEventListener("click", (event)=>{
+//   myForm.classList.toggle("show");
+
+// });
+// let checkoutForm = document.getElementById('checkout-form-form');
+//   checkoutForm.addEventListener("submit",function(event){
+//     console.log("how to add event")
+//     event.preventDefault();
+    
+//     })
+
+let cartButton = document.getElementById('showCartButton');
+cartButton.addEventListener("click", (event)=>{
+  totalcart.classList.toggle("hidden");
+
+});
+
+
